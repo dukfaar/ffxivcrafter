5b91083ca50ccd40132d73b0ac89b9dbd627d44e
Robert Schumann <dukfaar@gmail.com>
2016-09-08 09:50:40 +0200

    changed application name and title

--- config/env/development.js
+++ config/env/development.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   hostname: 'http://localhost:3000',
   app: {
-    name: 'FFXIVCrafter - DEV MODE'
+    name: 'RainCollector - DEV MODE'
   },
   strategies: {
     local: {
--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/ffxivcrafter',
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
@@ -28,7 +28,7 @@ module.exports = {
   },
   hostname: 'http://localhost:3000',
   app: {
-    name: 'FFXIVCrafter'
+    name: 'RainCollector'
   },
   logging: {
     format: 'combined'
