7c37c49dbd0f77049a772d798b49fc794c9fb38a
Robert Schumann <dukfaar@gmail.com>
2016-09-29 13:12:27 +0200

    fixed the fix of the fix, hope ^.^ code is still ugly

--- packages/custom/ffxivcrafter/public/services/project.js
+++ packages/custom/ffxivcrafter/public/services/project.js
@@ -74,31 +74,48 @@ angular.module('mean.ffxivCrafter').factory('projectAnalyzerService', function (
     projectData.totalCost += stepPrice(step)
   }
 
+  function getMaxCraftableSteps (step, projectData) {
+    var result = {}
+
+    result.neededAmount = step.amount - getAmountOfItemInUnnallocatedStock(projectData, step.item._id, step.hq)
+
+    result.neededSteps = Math.ceil(result.neededAmount / step.recipe.outputs[0].amount) // how often we need to craft the recipe to fulfill the need
+    result.maxSteps = result.neededSteps // how often we can craft the recipe, with our input materials
+
+    result.neededInputs = {}
+
+    step.recipe.inputs.forEach(function (input, index) {
+      var neededItems = input.amount * result.neededSteps
+
+      var itemsInStock = getAmountOfItemInUnnallocatedStock(projectData, input.item, step.inputs[index].hq) // 50
+      var remainingNeeded = neededItems - itemsInStock
+
+      result.neededInputs[input.item] = remainingNeeded
+      var possibleSteps = itemsInStock > 0 ? itemsInStock / input.amount : 0
+
+      result.maxSteps = Math.min(result.maxSteps, possibleSteps)
+    })
+
+    return result
+  }
+
+  // rewrite this thingy
   function craftingStep (step, projectData) {
-    var neededAmount = step.amount - getAmountOfItemInUnnallocatedStock(projectData, step.item._id, step.hq)
+    console.log('craft ' + step.item.name + ' start')
 
-    if (neededAmount > 0) {
-      var neededSteps = Math.ceil(neededAmount / step.recipe.outputs[0].amount) // how often we need to craft the recipe to fulfill the need
-      var maxSteps = neededSteps // how often we can craft the recipe, with our input materials
+    var stepData = getMaxCraftableSteps(step, projectData)
+    console.log('can craft a maximum of %i', stepData.maxSteps)
 
-      var neededInputs = {}
+    if (stepData.neededAmount > 0) {
+      // we need to craft
 
-      step.recipe.inputs.forEach(function (input, index) {
-        var neededItems = input.amount * neededSteps
+      if (stepData.maxSteps > 0) {
+        // and we can craft at least a few things
 
-        var itemsInStock = getAmountOfItemInUnnallocatedStock(projectData, input.item, step.inputs[index].hq)
-        var remainingNeeded = neededItems - itemsInStock
-
-        neededInputs[input.item] = remainingNeeded
-        var possibleSteps = itemsInStock > 0 ? itemsInStock / input.amount : 0
-
-        maxSteps = Math.min(maxSteps, possibleSteps)
-      })
-
-      if (maxSteps > 0) {
+        // create a crafting step
         projectData.craftableSteps.push({
           step: {
-            amount: maxSteps * step.recipe.outputs[0].amount,
+            amount: stepData.maxSteps * step.recipe.outputs[0].amount,
             item: step.item,
             recipe: step.recipe,
             inputs: $.extend(true, {}, step.inputs),
@@ -106,20 +123,42 @@ angular.module('mean.ffxivCrafter').factory('projectAnalyzerService', function (
           }
         })
 
+        // and remove every item that would be used in this crafting steps
         step.recipe.inputs.forEach(function (input, index) {
-          deductFromUnallocatedStock(projectData, input.item, input.amount * maxSteps, step.inputs[index].hq)
+          console.log('allocated %i %s', input.amount * stepData.maxSteps, input.item)
+          deductFromUnallocatedStock(projectData, input.item, input.amount * stepData.maxSteps, step.inputs[index].hq)
         })
+      } else {
+        // we still need to allocate any items that COULD be used
+        step.inputs.forEach(function (input, index) {
+          var itemsInStock = getAmountOfItemInUnnallocatedStock(projectData, input.item._id, input.hq)
+          var amount = Math.min(itemsInStock, input.amount)
+          deductFromUnallocatedStock(projectData, input.item._id, amount, input.hq)
+        })
+      /*step.recipe.inputs.forEach(function (input, index) {
+        console.log('allocated %i %s', input.amount * stepData.maxSteps, input.item)
+        deductFromUnallocatedStock(projectData, input.item, input.amount * stepData.maxSteps, step.inputs[index].hq)
+      })*/
       }
 
       step.inputs.forEach(function (input, index) {
-        input.amount = neededInputs[input.item._id]
+        input.amount = stepData.neededInputs[input.item._id]
+        console.log('input of %s set to %i', input.item.name, input.amount)
         analyzeStep(input, projectData)
       })
     } else {
+      /*
+      no crafting needed
+      deduce this steps items from the stock and be done
+      */
+      console.log('deducing %i items', step.amount)
       deductFromUnallocatedStock(projectData, step.item._id, step.amount, step.hq)
     }
   }
 
+  /**
+    have a look at this, was the simple version enough?
+  */
   function metaStep (step, projectData) {
     step.inputs.forEach(function (input, index) {
       if (input.step !== 'Meta') {
