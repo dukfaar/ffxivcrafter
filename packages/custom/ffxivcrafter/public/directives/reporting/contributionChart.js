f9f07112ac2166f0781b6dc35dc32a112fa2fd56
Robert Schumann <dukfaar@gmail.com>
2017-02-03 14:51:18 +0100

    formula for contribution is now editable

--- packages/custom/ffxivcrafter/public/directives/reporting/contributionChart.js
+++ packages/custom/ffxivcrafter/public/directives/reporting/contributionChart.js
@@ -5,7 +5,7 @@ angular.module('mean.ffxivCrafter').directive('reportingUserContributionChart', 
     templateUrl: '/ffxivCrafter/views/reporting/userContributionChart.html',
     scope: {
     },
-    controller: function ($scope, _) {
+    controller: function ($scope, _, localStorageService) {
       $scope.chart = {
         data: [],
         labels: [],
@@ -14,19 +14,64 @@ angular.module('mean.ffxivCrafter').directive('reportingUserContributionChart', 
         dataArray: []
       }
 
+      $scope.lastLog = []
+
       $scope.config = {
         showDiagram: false
       }
+
+      $scope.functionList = {
+        gatheringLevel: (logEntry, stack) => stack.push(logEntry.item.gatheringLevel),
+        gatheringEffort: (logEntry, stack) => stack.push(logEntry.item.gatheringEffort),
+        craftingLevel: (logEntry, stack) => stack.push(logEntry.recipe ? logEntry.recipe.craftingLevel : 0),
+        amount: (logEntry, stack) => stack.push(logEntry.amount),
+
+        one: (logEntry, stack) => stack.push(1),
+        zero: (logEntry, stack) => stack.push(0),
+
+        log: (logEntry, stack) => stack.push(Math.log(stack.pop())),
+        multiply: (logEntry, stack) => stack.push(stack.pop() * stack.pop()),
+        add: (logEntry, stack) => stack.push(stack.pop() + stack.pop()),
+        floor: (logEntry, stack) => stack.push(Math.floor(stack.pop())),
+        ceil: (logEntry, stack) => stack.push(Math.ceil(stack.pop()))
+      }
+
+      $scope.functionDescriptors = _.mapValues($scope.functionList, (value, key) => { return {name: key} })
+
+      if (localStorageService.get('reporting.contributionFunction') === null) {
+        localStorageService.set('reporting.contributionFunction', [
+          { name: 'gatheringLevel' },
+          { name: 'craftingLevel' },
+          { name: 'add' },
+          { name: 'gatheringEffort' },
+          { name: 'add' },
+          { name: 'log'},
+          { name: 'amount' },
+          { name: 'multiply' }
+        ])
+      }
+
+      $scope.functionData = {
+        showFormula: false,
+        list: localStorageService.get('reporting.contributionFunction')
+      }
+
+      $scope.$watch('functionData.list', () => {
+        localStorageService.set('reporting.contributionFunction', $scope.functionData.list)
+        $scope.updateGraph($scope.lastLog)
+      }, true)
 
       $scope.updateGraph = function (log) {
         var groupedByUser = _.groupBy(log, function (logItem) { return logItem.submitter.name })
         var countedByUser = _.mapValues(groupedByUser, function (userLogs) {
           return _.reduce(userLogs, function (sum, logEntry) {
-            var logValue = 0
-            var levelValue = logEntry.item.gatheringLevel + logEntry.item.gatheringEffort
-            if(logEntry.recipe) levelValue += logEntry.recipe.craftingLevel
-            if(logEntry.amount > 0) logValue = logEntry.amount * (Math.log(levelValue))
-            return sum + logValue
+            var stack = []
+            if (logEntry.amount > 0) {
+              _.forEach($scope.functionData.list, function (mod) {
+                $scope.functionList[mod.name](logEntry, stack)
+              })
+            } else stack.push(0)
+            return sum + (stack.length === 1 ? stack.pop() : NaN)
           }, 0)
         })
         $scope.chart.data = _.values(countedByUser)
@@ -35,7 +80,10 @@ angular.module('mean.ffxivCrafter').directive('reportingUserContributionChart', 
         $scope.chart.summedContribution = _.reduce($scope.chart.data, function (sum, d) { return sum + d }, 0)
       }
 
-      $scope.$on('stockchangelog was refiltered', function (event, data) { $scope.updateGraph(data) })
+      $scope.$on('stockchangelog was refiltered', function (event, data) {
+        $scope.lastLog = data
+        $scope.updateGraph($scope.lastLog)
+      })
     }
   }
 })
