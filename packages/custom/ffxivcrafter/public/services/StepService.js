60784880978934e7c83dbf9910ba5ab3b32b8595
Robert Schumann <dukfaar@gmail.com>
2017-02-09 10:34:33 +0100

    canMan and canCraft now take Folklore and Masterbooks into account

--- packages/custom/ffxivcrafter/public/services/StepService.js
+++ packages/custom/ffxivcrafter/public/services/StepService.js
@@ -4,14 +4,22 @@ angular.module('mean.ffxivCrafter').factory('StepService', [ 'MeanUser',
   function (MeanUser) {
     return {
       canHarvest: function (step) {
-        var map = [['Miner', 'minerLevel'], ['Botanist', 'botanistLevel']]
+        var map = [['Miner', 'miner'], ['Botanist', 'botanist']]
 
         for (var i in map) {
           var job = map[i]
           if (step.item.gatheringJob === 'None') {
             return true
           } else if (step.item.gatheringJob === job[0]) {
-            return step.item.gatheringLevel <= MeanUser.user[job[1]]
+            var hasFolklore = true
+            if (step.item.unspoiledNodeTime && step.item.unspoiledNodeTime.folkloreNeeded && step.item.unspoiledNodeTime.folkloreNeeded.length > 0) {
+              if (MeanUser.user[job[1] + 'Folklore']) {
+                hasFolklore = MeanUser.user[job[1] + 'Folklore'][step.item.unspoiledNodeTime.folkloreNeeded] === true
+              } else {
+                hasFolklore = false
+              }
+            }
+            return hasFolklore && step.item.gatheringLevel <= MeanUser.user[job[1] + 'Level']
           }
         }
 
@@ -20,21 +28,29 @@ angular.module('mean.ffxivCrafter').factory('StepService', [ 'MeanUser',
 
       canCraft: function (step) {
         var map = [
-        ['Weaver', 'weaverLevel'],
-        ['Culinarian', 'culinarianLevel'],
-        ['Alchemist', 'alchimistLevel'],
-        ['Blacksmith', 'blacksmithLevel'],
-        ['Carpenter', 'carpenterLevel'],
-        ['Armorer', 'armorerLevel'],
-        ['Goldsmith', 'goldsmithLevel'],
-        ['Leatherworker', 'leatherworkerLevel']
+        ['Weaver', 'weaver'],
+        ['Culinarian', 'culinarian'],
+        ['Alchemist', 'alchimist'],
+        ['Blacksmith', 'blacksmith'],
+        ['Carpenter', 'carpenter'],
+        ['Armorer', 'armorer'],
+        ['Goldsmith', 'goldsmith'],
+        ['Leatherworker', 'leatherworker']
         ]
 
         for (var i in map) {
           var job = map[i]
 
           if (step.step.recipe.craftingJob === job[0]) {
-            return step.step.recipe.craftingLevel <= MeanUser.user[job[1]]
+            var hasMaster = true
+            if (step.step.recipe.masterbook > 0) {
+              if (MeanUser.user[job[1] + 'Master']) {
+                hasMaster = MeanUser.user[job[1] + 'Master'][(step.step.recipe.masterbook - 1)] === true
+              } else {
+                hasMaster = false
+              }
+            }
+            return hasMaster && step.step.recipe.craftingLevel <= MeanUser.user[job[1] + 'Level']
           }
         }
 
