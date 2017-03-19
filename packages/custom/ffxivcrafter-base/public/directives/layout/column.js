3cc2505c0d67ef6cc446d550114a1bcad87f9bea
Robert Schumann <robert.schumann@unic.com>
2017-03-19 14:59:39 +0100

    moved layout templates into seperate files

--- packages/custom/ffxivcrafter-base/public/directives/layout/column.js
+++ packages/custom/ffxivcrafter-base/public/directives/layout/column.js
@@ -1,17 +1,10 @@
 'use strict'
 
-angular.module('mean.ffxivCrafter_base').directive('column', function () {
+angular.module('mean.ffxivCrafter_base')
+.directive('column', function () {
   return {
     transclude: true,
     replace: true,
-    template: '<div layout="column" flex="25" flex-xs="100" flex-sm="50" ng-transclude></div>'
-  }
-})
-
-angular.module('mean.ffxivCrafter').directive('columnLayout', function () {
-  return {
-    transclude: true,
-    replace: true,
-    template: '<div layout-wrap flex layout="row" layout-xs="column" layout-sm="row" ng-transclude></div>'
+    templateUrl: '/ffxivCrafter_base/views/layout/column.html'
   }
 })
--- packages/custom/ffxivcrafter-base/public/directives/layout/columnLayout.js
+++ packages/custom/ffxivcrafter-base/public/directives/layout/columnLayout.js
@@ -1,10 +1,10 @@
 'use strict'
 
 angular.module('mean.ffxivCrafter_base')
-.directive('pageBase', function () {
+.directive('columnLayout',function () {
   return {
     transclude: true,
     replace: true,
-    templateUrl: '/ffxivCrafter_base/views/layout/pageBase.html'
+    templateUrl: '/ffxivCrafter_base/views/layout/columnLayout.html',
   }
 })
