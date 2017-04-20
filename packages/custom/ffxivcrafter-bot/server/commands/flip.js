cc64facf590a1f6c1ac58e70b942e95e26a25ec8
Robert Schumann <robert.schumann@unic.com>
2017-04-20 23:57:47 +0200

    proper flips

--- packages/custom/ffxivcrafter-bot/server/commands/flip.js
+++ packages/custom/ffxivcrafter-bot/server/commands/flip.js
@@ -1,14 +1,27 @@
 'use strict'
 
+let _ = require('lodash')
+
 module.exports = function (botDef) {
   return {
     name: 'flip',
     command: command
   }
 
+  function flipText (text) {
+    let char = 'abcdefghijklmnopqrstuvwxyz'
+    let flipChar = 'ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz'
+
+    return _.map(text, c => {
+      let i = char.indexOf(_.lowerCase(c))
+      return (i === -1) ? c : flipChar[i]
+    })
+  }
+
   function command (params, message) {
     if (params.length > 1 && params[1] !== botDef.commandTrigger) {
-      message.channel.sendMessage('(╯°□°）╯︵' + params[1])
+      let flipped = _.reverse(flipText(params[1])).join('')
+      message.channel.sendMessage('(╯°□°）╯︵' + flipped + '︵' + params[1] + '︵' + flipped)
     } else {
       message.channel.sendMessage('You flipped me? How dare you! I informed my master about that!')
     }
