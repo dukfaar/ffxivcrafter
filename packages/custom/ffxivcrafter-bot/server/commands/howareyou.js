2f38d7697ee125aef76fe1d6773817cb52899b72
Robert Schumann <robert.schumann@unic.com>
2017-04-27 10:08:13 +0200

    new how are you command

---
+++ packages/custom/ffxivcrafter-bot/server/commands/howareyou.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var mongoose = require('mongoose')
+var _ = require('lodash')
+var q = require('q')
+var os = require('os')
+var process = require('process')
+
+module.exports = function (botDef) {
+  return {
+    name: 'how are you',
+    command: command
+  }
+
+  var UserDiscord
+
+  function command (params, message) {
+    UserDiscord = UserDiscord || mongoose.model('UserDiscord')
+
+    let name = _.join(_.slice(params, 2), ' ')
+
+    UserDiscord.find().lean().exec().then((discordUsers) => {
+      let sentences = []
+
+      let totalMurders = _.reduce(discordUsers, (sum, u) => { return sum + u.murderAttempts }, 0)
+
+      sentences.push('My house was built ' + os.uptime() + ' seconds ago')
+      sentences.push('I am awake since ' + process.uptime() + ' seconds')
+
+      let cpus = os.cpus()
+      let totalThinks = _.reduce(cpus, (sum, c) => { return sum + c.speed }, 0)
+
+      sentences.push('I have ' + cpus.length + ' brain(s) doing ' + totalThinks + ' million thinkings per second')
+      sentences.push('In the last 15 minutes i was thinking exactly(!) this hard: ' + os.loadavg()[2])
+
+      sentences.push('So far people tried to kill me ' + totalMurders + ' times.')
+
+      message.channel.sendMessage(_.join(sentences, '\n'))
+    })
+  }
+}
