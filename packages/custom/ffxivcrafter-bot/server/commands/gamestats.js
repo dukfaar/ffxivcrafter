4e1c19d278b8e96023af667b36b7fd7c20a73101
Robert Schumann <robert.schumann@unic.com>
2017-06-01 15:39:06 +0200

    added current farming status to gamestats

--- packages/custom/ffxivcrafter-bot/server/commands/gamestats.js
+++ packages/custom/ffxivcrafter-bot/server/commands/gamestats.js
@@ -10,16 +10,17 @@ module.exports = function (botDef) {
   }
 
   var UserDiscord
+  var GameStats
 
   function command (params, message) {
     UserDiscord = UserDiscord || mongoose.model('UserDiscord')
+    GameStats = GameStats || mongoose.model('GameStats')
 
     var discordName = message.author.username + '#' + message.author.discriminator
+    let sentences = []
 
     UserDiscord.findOne({discord: discordName}).lean().exec()
     .then(userDiscord => {
-      let sentences = []
-
       if(userDiscord) {
         sentences.push('You currently have ' + (userDiscord.gold || 0) + ' gold.')
         sentences.push('You have ' + (userDiscord.zombies || 0) + ' zombies.')
@@ -27,6 +28,21 @@ module.exports = function (botDef) {
         sentences.push('Sorry, i have no idea who you are.')
       }
 
+      return GameStats.findOne({user: userDiscord.user}).exec()
+    })
+    .then(gamestats => {
+      if (gamestats && gamestats.farmStart && gamestats.farmDuration) {
+        let now = new Date()
+        let nowMs = now.getTime()
+
+        let thenMs = gamestats.farmStart.getTime() + gamestats.farmDuration
+
+        let distance = thenMs - nowMs
+
+        sentences.push('You are currently farming potatos. Time to complete: ' + (distance / 1000) + ' seconds.')
+      }
+    })
+    .then(() => {
       message.channel.send(_.join(sentences, '\n'))
     })
   }
