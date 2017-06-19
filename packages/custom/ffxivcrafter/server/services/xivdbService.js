aca725d21271d555058987b9cfec0a3a8068686a
Robert Schumann <dukfaar@gmail.com>
2017-06-19 20:55:53 +0200

    added more loggin

--- packages/custom/ffxivcrafter/server/services/xivdbService.js
+++ packages/custom/ffxivcrafter/server/services/xivdbService.js
@@ -5,6 +5,8 @@ var httpreq = require('httpreq')
 
 let nextRequestTime = Date.now()
 
+var logger = require('log4js').getLogger('app.xivdb')
+
 module.exports = function () {
   function getData (url) {
     var deferred = Q.defer()
@@ -15,20 +17,24 @@ module.exports = function () {
     if (now > nextRequestTime) delay = 0
     else delay = nextRequestTime - now
 
-    nextRequestTime = now + delay + 200
+    nextRequestTime = now + delay + 500
 
     Q.delay(delay)
     .then(function () {
+      logger.info('sending import request for url ' + url)
       httpreq.get(url, function (err, xivdata) {
         if (err) {
+          logger.error(err)
           deferred.reject(err)
         } else {
           var data
 
           try {
             data = JSON.parse(xivdata.body)
+            logger.info(xivdata.body)
             deferred.resolve(data)
           } catch (err) {
+            logger.error(err)
             deferred.reject(err)
           }
         }
